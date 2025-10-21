2be470ae7089ea8b523b975412d660ac4cb348c3
dokuzsertkol <120343813+dokuzsertkol@users.noreply.github.com>
2025-10-21 09:10:59 +0300

    redis

---
+++ backend/src/infrastructure/redis/cacheService.ts
@@ -0,0 +1,16 @@
+import redis from "./redisClient.js";
+
+export class CacheService {
+    async get<T>(key: string): Promise<T | null> {
+        const data = await redis.get(key);
+        return data ? JSON.parse(data) : null;
+    }
+
+    async set<T>(key: string, value: T, ttlSeconds?: number): Promise<void> {
+        if (ttlSeconds) {
+            await redis.set(key, JSON.stringify(value), "EX", ttlSeconds);
+        } else {
+            await redis.set(key, JSON.stringify(value));
+        }
+    }
+}
--- backend/src/infrastructure/repositories/settingsRepo.ts
+++ backend/src/infrastructure/repositories/settingsRepo.ts
@@ -1,11 +1,24 @@
 import { Settings } from "../../domain/models/settingsModel.js";
 import type { ISettings } from "../../domain/types/settings.js";
 import type { ISettingsRepo } from "./interfaces/ISettingsRepo.js";
+import { CacheService } from "../redis/cacheService.js";
 
 export class SettingsRepo implements ISettingsRepo {
+    private cacheService = new CacheService();
+    private cacheKey = "settings";
 
     async getSettings(): Promise<ISettings> {
+        const cached = await this.cacheService.get<ISettings>(this.cacheKey);
+        if (cached) {
+            console.log("redis");
+            return cached;
+        }
+        
         const config = await Settings.findOne({}).lean() as ISettings | null;
-        return config || { stateIntervalHours: 0, stateCount: 0 } as ISettings;
+        const settings = config || { stateIntervalHours: 0, stateCount: 0 } as ISettings;
+
+        await this.cacheService.set<ISettings>(this.cacheKey, settings, 3600 * 4);
+        
+        return settings;
     };
 };
